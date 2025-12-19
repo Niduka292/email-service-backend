f6b5c34317d866dd32ec11f085c5a957bf03fc66
Niduka Jayathunga <nidukajayathunga886@gmail.com>
2025-12-19 20:39:26 +0530

    Update AIService to generate smart replies

--- src/main/java/com/emailapp/emailservice/service/AIService.java
+++ src/main/java/com/emailapp/emailservice/service/AIService.java
@@ -1,11 +1,15 @@
 package com.emailapp.emailservice.service;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.genai.Client;
 import com.google.genai.types.GenerateContentResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.View;
+
+import java.util.List;
 
 @Service
 public class AIService {
@@ -42,6 +46,30 @@ public class AIService {
         }catch(Exception e){
             System.err.println("Gemini API error "+e.getMessage());
             return "Failed to generate summary due to an internal API error.";
+        }
+    }
+
+    public List<String> generateSmartReplies(String emailContent){
+        String prompt = String.format(
+                "Based on the following email, generate exactly 3 short, helpful, and professional " +
+                        "reply options (max 10 words each). " +
+                        "Return the response ONLY as a JSON array of strings. " +
+                        "Example: [\"Sounds good!\", \"I'll check and let you know.\", \"Can we reschedule?\"] " +
+                        "Email content: \n\n%s",
+                emailContent
+        );
+
+        try{
+            GenerateContentResponse response = geminiClient.models.generateContent(MODEL_NAME,prompt,null);
+            String rawText = response.text().trim();
+
+            String cleanJson = rawText.replace("```json", "").replace("```", "").trim();
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readValue(cleanJson, new TypeReference<List<String>>() {});
+        }catch(Exception e){
+            System.err.println("Smart reply error: "+e.getMessage());
+            return List.of("reply 1", "reply 2", "reply 3"); // Fallback
         }
     }
 
